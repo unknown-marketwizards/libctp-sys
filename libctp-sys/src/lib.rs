63756f4e76c4c756f1d6dc7055b53d4cc46c7041
themarketwizard <marketwizard26@gmail.com>
2024-05-08 23:08:40 +0800

    linux

--- libctp-sys/src/lib.rs
+++ libctp-sys/src/lib.rs
@@ -2,12 +2,20 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[cfg(target_os = "linux")]
+mod bindings_linux;
+
+#[cfg(target_os = "linux")]
+pub use bindings_linux::*;
+
+#[cfg(target_os = "windows")]
 mod bindings;
+
+#[cfg(target_os = "windows")]
+pub use bindings::*;
 
 unsafe impl Send for Rust_CThostFtdcMdApi {}
 unsafe impl Sync for Rust_CThostFtdcMdApi {}
 
 unsafe impl Send for Rust_CThostFtdcTraderApi {}
 unsafe impl Sync for Rust_CThostFtdcTraderApi {}
-
-pub use bindings::*;
